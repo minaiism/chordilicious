f7ed50e49e4fa446f8a879d4719c42fcd6644dcb
minaiism <martyna.wojcik.mail@gmail.com>
2019-08-23 17:39:03 +0200

    add search-bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from "./components/NavBar/NavBar";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 class App extends React.Component {
     render() {
         return (
             <div>
                 <NavBar/>
+                <SearchBar/>
             </div>
         );
     }
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Input from "@material-ui/core/Input";
+
+const useStyles = makeStyles(theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '2rem'
+    },
+    input: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+    }
+}));
+
+const SearchBar = () => {
+    const classes = useStyles();
+    const [values, setValues] = React.useState({
+        input: ''
+    });
+
+    return (
+        <form className={classes.container} noValidate autoComplete="off">
+            <Input
+                className={classes.input}
+                id="input-with-icon-adornment"
+                startAdornment={
+                    <InputAdornment position="start">
+                        <SearchIcon />
+                    </InputAdornment>
+                }
+            />
+        </form>
+    );
+};
+
+export default SearchBar;
