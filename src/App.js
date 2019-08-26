eb3fe07fdf5c1cb2a4607456a0cc7238dffe99d2
minaiism <martyna.wojcik.mail@gmail.com>
2019-08-26 13:08:52 +0200

    add prettier

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 import SearchBar from "./components/SearchBar/SearchBar";
+import Footer from "./components/Footer/Footer";
+import "./App.css";
 
 class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <NavBar/>
-                <SearchBar/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className={App}>
+        <NavBar />
+        <SearchBar />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
