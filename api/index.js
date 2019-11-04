3dab8a944f0b9f75dfc4ec6ba3549ede5f21b540
minaiism <martyna.wojcik.mail@gmail.com>
2019-11-04 22:30:37 +0100

    add config lib && set jsonwebtoken

--- api/index.js
+++ api/index.js
@@ -1,37 +1,41 @@
-import Users from './routes/Users'
-import Auth from "./routes/Auth";
+import Users from './routes/Users';
+import Auth from './routes/Auth';
+import config from 'config';
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
+/*dotenv config*/
 const dotenv = require('dotenv');
 dotenv.config();
+/*mongo env config */
 const mongoUri = process.env.MONGO_URI;
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//Set up default mongoose connection
+if (!config.get('PrivateKey')) {
+  console.error('FATAL ERROR: PrivateKey is not defined.');
+  process.exit(1);
+}
+
+/*Set up mongoose connection*/
 mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
 })
-    .then(() => console.log('Now connected to MongoDB!'))
-    .catch(error => { console.log('caught', error.message)});
-
-/* Get the default connection */
-const db = mongoose.connection;
-
-/* Bind connection to error event (to get notification of connection errors) */
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+  .then(() => console.log('Now connected to MongoDB!'))
+  .catch(error => {
+    console.log('caught', error.message);
+  });
 
 app.use(express.json());
 app.use('/users', Users);
 app.use('/auth', Auth);
 
 app.listen(port, () => {
-    console.log('App listening on port: ' + port);
+  console.log('App listening on port: ' + port);
 });
