0f78d3dc37214363b988e13df70a75127efb4278
minaiism <martyna.wojcik.mail@gmail.com>
2020-01-19 16:40:32 +0100

    add spinner to auth-wrapper component

--- client/src/components/Auth/AuthWrapper.js
+++ client/src/components/Auth/AuthWrapper.js
@@ -1,10 +1,22 @@
 import { useUserContext } from '../Context/Context';
 import React, { useEffect } from 'react';
 import ApiClient from '../../services/ApiClient';
-import SnackBarPane from '../Views/ContentPanes/SnackBarPane/SnackBarPane';
+import { CircularProgress, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  spinner: {
+    fontSize: '6rem'
+  }
+});
 
 const AuthWrapper = ({ pane }) => {
-  const { user, setUser, setError} = useUserContext();
+  const { user, setUser, setError } = useUserContext();
+  const classes = useStyles();
 
   useEffect(() => {
     if (user === null) {
@@ -18,7 +30,8 @@ const AuthWrapper = ({ pane }) => {
     }
   }, [user, setUser, setError]);
 
-  return user ? (<div>{pane}</div>): (<SnackBarPane/>);
+  return user ? (<div>{pane}</div>) : (
+    <article className={classes.container}><CircularProgress className={classes.spinner}/></article>);
 };
 
 export default AuthWrapper;
