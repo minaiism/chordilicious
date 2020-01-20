86afeaf179360d15b87290fc70701717f5697690
minaiism <martyna.wojcik.mail@gmail.com>
2020-01-20 13:30:10 +0100

    fix spinner & snackbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import MainView from './components/Views/MainView';
 import { useUserContext } from './components/Context/Context';
 
-
 const App = () => (
   <useUserContext.Provider>
     <MainView/>
--- client/src/components/Auth/AuthWrapper.js
+++ client/src/components/Auth/AuthWrapper.js
@@ -1,37 +1,45 @@
 import { useUserContext } from '../Context/Context';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ApiClient from '../../services/ApiClient';
+import SnackBarPane from '../Views/ContentPanes/SnackBarPane/SnackBarPane';
 import { CircularProgress, makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   container: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center'
   },
-  spinner: {
+  spinnerIcon: {
     fontSize: '6rem'
   }
-});
+}));
 
 const AuthWrapper = ({ pane }) => {
-  const { user, setUser, setError } = useUserContext();
   const classes = useStyles();
+  const { user, setUser, setError } = useUserContext();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    if (user === null) {
+    if (user === null && isLoggedIn === false) {
       ApiClient.get('/users/me')
         .then(res => {
           setUser(res.data);
+          setIsLoggedIn(true);
+          setIsLoading(true);
         })
         .catch(err => {
           setError(err.message);
+          setIsLoggedIn(false);
+          setIsLoading(false);
         });
     }
-  }, [user, setUser, setError]);
+  }, [user, setUser, setError, isLoggedIn, isLoading]);
 
-  return user ? (<div>{pane}</div>) : (
-    <article className={classes.container}><CircularProgress className={classes.spinner}/></article>);
+  return isLoading ? user ? <div>{pane}</div> : (
+    <article className={classes.container}><CircularProgress className={classes.spinnerIcon}/></article>) : (
+    <SnackBarPane/>);
 };
 
 export default AuthWrapper;
