58f461f3de4243455a1337fc33ef86f36988170b
Roman Segeda <roman.segeda.dev@gmail.com>
2020-06-28 19:59:40 +0200

    #68j0mx Add eslint and prettier in client 2

--- client/src/__tests__/App.test.jsx
+++ client/src/__tests__/App.test.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
 
-/**
- * @jest-environment jsdom
- */
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
